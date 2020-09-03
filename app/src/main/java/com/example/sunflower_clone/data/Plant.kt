179a7b43128495c81e128d18183ab302c6c2e2d0
sery270 <sery270@naver.com>
2020-09-03 18:41:50 +0900

    [Add Codes] fragment_plant_detail.xml의 data인 Plant.kt 작성

---
+++ app/src/main/java/com/example/sunflower_clone/data/Plant.kt
@@ -0,0 +1,22 @@
+package com.example.sunflower_clone.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Calendar
+import java.util.Calendar.DAY_OF_YEAR
+
+@Entity(tableName = "plants")
+data class Plant(
+    @PrimaryKey @ColumnInfo(name = "id") val plantId: String,
+    val name: String,
+    val description: String,
+    val growZoneNumber: String,
+    val wateringInterval: Int = 7, // the number means days interval
+    val imageUrl: String = ""
+){
+    fun shouldBeWatered(since: Calendar, lastWateringDate: Calendar) =
+        since > lastWateringDate.apply { add(DAY_OF_YEAR, wateringInterval) }
+
+    override fun toString() = name
+}
