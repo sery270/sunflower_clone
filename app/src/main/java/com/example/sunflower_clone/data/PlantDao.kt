576bc029062bc71e6e06478cb99d65466fbda1d5
sery270 <sery270@naver.com>
2020-09-09 17:56:58 +0900

    [Add Codes] PlantDao.kt 작성

---
+++ app/src/main/java/com/example/sunflower_clone/data/PlantDao.kt
@@ -0,0 +1,22 @@
+package com.example.sunflower_clone.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PlantDao {
+    @Query("SELECT * FROM plants ORDER BY name")
+    fun getPlants(): LiveData<List<Plant>>
+
+    @Query("SELECT * FROM plants WHERE growZoneNumber = :growZoneNumber ORDER BY name")
+    fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): LiveData<List<Plant>>
+
+    @Query("SELECT * FROM plants WHERE id = :plantId")
+    fun getPlant(plantId: String): LiveData<Plant>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(plants: List<Plant>)
+}
